ab215546abb3c774b0695bc9ad83916099852e13
Erwan Thoury <erwan.thoury@gmail.com>
2023-03-28 11:59:53 +0200

    ajout

--- projetDex/src/main/java/com/mycompany/projetdex/Pokemon.java
+++ projetDex/src/main/java/com/mycompany/projetdex/Pokemon.java
@@ -18,9 +18,74 @@ public class Pokemon {
     
     // Constructeurs à faire (Plusieurs car un pokemon peut avoir un ou plusieurs type.
     // Un pokemon peut avoir une évolution supérieur et/ou une inférieur.
-    
-    public Pokemon getEvolutionSup()
+    public Pokemon(String nom, int numeroPokedex, Element element1, Element element2, Pokemon pokemonSupérieur)
     {
+        this.nom = nom;
+        this.numeroPokedex = numeroPokedex;
+        this.element1 = element1;
+        this.element2 = element2;
+        this.pokemonSupérieur = pokemonSupérieur;
+    } // Cas Bulbizarre
+    
+    public Pokemon(String nom, int numeroPokedex, Element element1, Element element2, Pokemon pokemonSupérieur, Pokemon pokemonInferieur)
+    {
+        this.nom = nom;
+        this.numeroPokedex = numeroPokedex;
+        this.element1 = element1;
+        this.element2 = element2;
+        this.pokemonSupérieur = pokemonSupérieur;
+        this.pokemonInferieur = pokemonInferieur;
+    } // Cas Herbizarre
+   
+    public Pokemon(String nom, int numeroPokedex, Element element1, Pokemon pokemonInferieur, Element element2)
+    {
+        this.nom = nom;
+        this.numeroPokedex = numeroPokedex;
+        this.element1 = element1;
+        this.element2 = element2;
+        this.pokemonInferieur = pokemonInferieur;
+    } // Cas Florizarre
+    public Pokemon(String nom, int numeroPokedex, Element element1, Pokemon pokemonSupérieur, Pokemon pokemonInferieur)
+    {
+        this.nom = nom;
+        this.numeroPokedex = numeroPokedex;
+        this.element1 = element1;
+        this.pokemonSupérieur = pokemonSupérieur;
+        this.pokemonInferieur = pokemonInferieur;
+    } // Cas Reptincelle
+    
+    public Pokemon(String nom, int numeroPokedex, Element element1, Pokemon pokemonSupérieur)
+    {
+        this.nom = nom;
+        this.numeroPokedex = numeroPokedex;
+        this.element1 = element1;
+        this.pokemonSupérieur = pokemonSupérieur;
+    } // Cas Salameche
+    
+    public Pokemon(String nom, int numeroPokedex, Pokemon pokemonInferieur, Element element1 )
+    {
+        this.nom = nom;
+        this.numeroPokedex = numeroPokedex;
+        this.element1 = element1;
+        this.pokemonInferieur = pokemonInferieur;
+    } // Cas Dracaufeu
+    
+    public Pokemon(String nom, int numeroPokedex, Element element1, Element element2 )
+    {
+        this.nom = nom;
+        this.numeroPokedex = numeroPokedex;
+        this.element1 = element1;
+        this.element2 = element2;
+    } // Cas Locklass
+    
+    public Pokemon(String nom, int numeroPokedex, Element element1)
+    {
+        this.nom = nom;
+        this.numeroPokedex = numeroPokedex;
+        this.element1 = element1;
+    } // Cas Insolourdo
+    
+    public Pokemon getEvolutionSup() {
         return null; // Retourne l'évolution supérieur
     }
     public Pokemon getEvolutionInf()
